c11f3d65abb2444e82b365d9d28a289187a74356
runningtalus <dave@davemay.org>
2014-03-17 12:31:14 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var firstReverse = function(userString){
+	var newArr = userString.split('');
+	return newArr.reverse().join('');
+};
+console.log(firstReverse('!sseniuG ym ,ssendoog yM'));
+console.log(firstReverse('!tluciffid os siht gnikam uoy era yhW'));
